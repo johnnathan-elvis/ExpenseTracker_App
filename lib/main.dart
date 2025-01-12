c792e50448e25bf784f7c892f7f0b3264fe4484a
johnnathan <johnnathanfreire@gmail.com>
2025-01-12 09:29:26 -0300

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
 import 'package:flutter_application_1/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
