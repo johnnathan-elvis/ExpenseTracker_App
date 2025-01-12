1e6ef1be4b10cba706d01a2ba97111aa1c372a68
johnnathan <johnnathanfreire@gmail.com>
2025-01-12 09:27:52 -0300

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -12,61 +12,6 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  // ]).then((fn) {
-  //   runApp(
-  //     MaterialApp(
-  //       darkTheme: ThemeData.dark().copyWith(
-  //         colorScheme: kDarkColorScheme,
-  //         cardTheme: const CardTheme().copyWith(
-  //           color: kDarkColorScheme.secondaryContainer,
-  //           margin: const EdgeInsets.symmetric(
-  //             horizontal: 16,
-  //             vertical: 8,
-  //           ),
-  //         ),
-  //         elevatedButtonTheme: ElevatedButtonThemeData(
-  //           style: ElevatedButton.styleFrom(
-  //             backgroundColor: kDarkColorScheme.primaryContainer,
-  //             foregroundColor: kDarkColorScheme.onPrimaryContainer,
-  //           ),
-  //         ),
-  //       ),
-  //       theme: ThemeData().copyWith(
-  //         colorScheme: kColorScheme,
-  //         appBarTheme: const AppBarTheme().copyWith(
-  //           backgroundColor: kColorScheme.onPrimaryContainer,
-  //           foregroundColor: kColorScheme.primary,
-  //           titleTextStyle: const TextStyle(color: Colors.white),
-  //         ),
-  //         cardTheme: const CardTheme().copyWith(
-  //           color: kColorScheme.secondaryContainer,
-  //           margin: const EdgeInsets.symmetric(
-  //             horizontal: 16,
-  //             vertical: 8,
-  //           ),
-  //         ),
-  //         elevatedButtonTheme: ElevatedButtonThemeData(
-  //           style: ElevatedButton.styleFrom(
-  //             backgroundColor: kColorScheme.primaryContainer,
-  //           ),
-  //         ),
-  //         textTheme: ThemeData().textTheme.copyWith(
-  //               titleLarge: TextStyle(
-  //                 fontWeight: FontWeight.normal,
-  //                 color: kColorScheme.onSecondaryContainer,
-  //                 fontSize: 14,
-  //               ),
-  //             ),
-  //       ),
-  //       debugShowCheckedModeBanner: false,
-  //       themeMode: ThemeMode.system,
-  //       home: const Expenses(),
-  //     ),
-  //   );
-  // });
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
